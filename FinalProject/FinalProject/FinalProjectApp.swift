7f9a79e2507067754ea37b72e085bff9c722790a
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-08-31 10:16:20 +0300

    project

---
+++ FinalProject/FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by الوليد خشيم on 15/02/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
