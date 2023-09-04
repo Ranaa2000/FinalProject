3250c060bc7d67de2977cf57fbbfacdd79e1d84a
Ranaa2000 <raanosh12@gmail.com>
2023-09-04 11:15:13 +0300

    add tab view

---
+++ FinalProject/FinalProject/View/TabView.swift
@@ -0,0 +1,58 @@
+//
+//  TabView.swift
+//  FinalProject
+//
+//  Created by Rana MHD on 19/02/1445 AH.
+//
+
+import SwiftUI
+
+struct TabdView: View {
+    var body: some View {
+        
+        TabView{
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(Color("blueC") ,for: .tabBar)
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(Color("blueC") ,for: .tabBar)
+            
+            // change the name into the the view name
+            SearchView()
+                .tabItem {
+                    Label("Post", systemImage: "plus")
+                }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(Color("blueC") ,for: .tabBar)
+            
+            SearchView()
+                .tabItem {
+                    Label("Post", systemImage: "heart")
+                }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(Color("blueC") ,for: .tabBar)
+            
+            SearchView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+                .toolbar(.visible, for: .tabBar)
+                .toolbarBackground(Color("blueC") ,for: .tabBar)
+        }
+        
+    }
+}
+
+struct TabBarViews_Previews: PreviewProvider {
+    static var previews: some View {
+        TabdView()
+    }
+}
