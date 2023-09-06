5658b055fc1e2eb1c1ff1bbfa1d24be60012ce54
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-09-06 22:06:59 +0300

    new progict

---
+++ BoksBox/BoksBox/BoksBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoksBoxApp.swift
+//  BoksBox
+//
+//  Created by الوليد خشيم on 21/02/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BoksBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoksBox/BoksBox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BoksBox
+//
+//  Created by الوليد خشيم on 21/02/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
